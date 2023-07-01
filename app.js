b0bea6eca7cb22569e21722ff404c0d4d3911d95
Tina <tina.winnn@gmail.com>
2023-07-01 13:50:24 -0400

    grabbed all dom elements needed

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-const alert = document.querySelector('alert');
+const submitBtn = document.querySelector('.submit-btn');
+const clearBtn = document.querySelector('.clear-btn');
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.to-do-form');
+const formTask = document.querySelector('.task');
+const taskContainer = document.querySelector('.to-do-container');
+const taskList = document.querySelector('.to-do-items');
+
+
+// edit option 
+
+// Event listeners 
+// submit form 
+// clear btn 
+// load items 
+
+// Functions 
+// Adding an item 
+
+// display the alert 
+
+// remove the alert 
+
+// clearitems 
+
+// delete single item 
+
+// edit func 
+
+// local storage 
+
